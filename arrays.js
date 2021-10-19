7e8e2c3026cdbeb1c5d56dcd0698e867eacd4b71
Safal Bhandari <82866677+Safal009@users.noreply.github.com>
2021-10-19 14:44:36 +0530

    Introduction to Array Methods (Jonas Schmedtmann)

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const friends = ['Micheal', 'Jim', 'Dwight']
+console.log(friends);
+friends.push('Andy'); //Adding Elements
+console.log(friends);
+friends.pop(friends.length-1) //Removing Elements
+console.log(friends)
+friends.pop(friends.length-1)
+console.log(friends)
+friends.pop(friends.length-1)
+console.log(friends)
+friends.unshift("Kevin") //Add at  first
+console.log(friends)
+friends.shift() //Remove the first element
+console.log(friends)
+console.log(friends.indexOf('Micheal')) //Find Index of
+console.log(friends.includes('Micheal')); //If Element is present
+console.log(friends.includes('Jim'));  
